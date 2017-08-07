e17a5eaae6f2d0c05be30eb7fd95d0e3aea0efed
Cagri Uslu <uslu.cagri@gmail.com>
2017-08-08 00:34:16 +0300

    First commit and 0.1.0 release

---
+++ include/utf8char.h
@@ -0,0 +1,94 @@
+/// \file
+/// This file defines utf8char_t, which is used by utf8str_t.
+/// \copyright Copyright 2017 Cagri Uslu. This library is released under the MIT License.
+
+#ifndef UTF8CHAR_H
+#define UTF8CHAR_H
+
+#include <stdint.h>
+#include <stdlib.h>
+
+/// \typedef utf8char_t
+/// \brief A type representing an UTF-8 character
+/// \details utf8char_t is actually a 32-bit unsigned number.
+/// The fixed size makes the processing easier.
+typedef uint32_t utf8char_t;
+
+/// \brief Returns the real size of a given utf8char_t (which might be 1, 2, 3, or 4)
+/// \return ((size_t) -1), if **c** is not a valid UTF-8 character.
+size_t utf8char_size(utf8char_t c)
+{
+	if (c < 128)
+		return 1;
+	else if (c < 57344 && 128 <= (c & 0x000000ff) && (c & 0x000000ff) < 192)
+		return 2;
+	else if (c < 15728640 && 128 <= (c & 0x000000ff) && (c & 0x000000ff) < 192 && 32768 <= (c & 0x0000ff00) && (c & 0x0000ff00) < 49152)
+		return 3;
+	else if (c < 4160749568 && 128 <= (c & 0x000000ff) && (c & 0x000000ff) < 192 && 32768 <= (c & 0x0000ff00) && (c & 0x0000ff00) < 49152 && 8388608 <= (c & 0x00ff0000) && (c & 0x00ff0000) < 12582912)
+		return 4;
+	else
+		return ((size_t) -1);
+}
+
+/// \brief Tries to deduce the real size of the UTF-8 character (which might be 1, 2, 3, or 4), by looking at its first byte
+/// \param b First byte of a UTF-8 character
+/// \return 0, if **b** is not the first byte of a valid UTF-8 character but might be one of the following bytes.
+/// \return ((size_t) -1), if **b** is definetly not a part of a valid UTF-8 character.
+size_t deduce_utf8char_size(uint8_t b)
+{
+	if (b < 128)
+		return 1;
+	else if (b < 192)
+		return 0;
+	else if (b < 224)
+		return 2;
+	else if (b < 240)
+		return 3;
+	else if (b < 248)
+		return 4;
+	else
+		return ((size_t) -1);
+}
+
+/// \brief Tries to parse a utf8char_t from the given string
+/// \return ((utf8char_t) -1), if the string does not contain a valid UTF-8 character at the beginning.
+utf8char_t str_to_utf8char(char *s)
+{
+	size_t deduced_size = deduce_utf8char_size(s[0]);
+	if (deduced_size == 0 || deduced_size == ((size_t) -1))
+		return ((utf8char_t) -1);
+
+	utf8char_t c = 0;
+	size_t i;
+	for (i = 0; i < deduced_size; i++)
+	{
+		uint32_t tmp = ((uint8_t) s[i]);
+		c |= tmp << (8 * (deduced_size - 1 - i));
+	}
+
+	if (utf8char_size(c) == ((size_t) -1))
+		return ((utf8char_t) -1);
+
+	return c;
+}
+
+/// \brief Writes a utf8char_t into a buffer
+/// \return The number of bytes written, if **c** is a valid UTF-8 character.
+/// \return ((size_t) -1), if **c** is not a valid UTF-8 character. The buffer is left untouched.
+size_t write_utf8char_to_buf(char *buf, utf8char_t c)
+{
+	size_t size = utf8char_size(c);
+	if (size == ((size_t) -1))
+		return size;
+
+	size_t i;
+	for (i = 0; i < size; i++)
+	{
+		uint8_t tmp = c >> (8 * (size - 1 - i));
+		buf[i] = tmp;
+	}
+
+	return size;
+}
+
+#endif
---
+++ include/utf8str.h
@@ -0,0 +1,207 @@
+/// \mainpage
+/// utf8str is a small library for easy UTF-8 encoded string processing.
+///
+/// \section Installation
+/// This library is a header-only library, just copy it anywhere your compiler can find.
+///
+/// \section Usage
+/// Read the documentation of the file "utf8str.h" and include it in your code.
+
+/// \file
+/// This file defines utf8str_t.
+
+#ifndef UTF8STR_H
+#define UTF8STR_H
+
+#include "utf8char.h"
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define UTF8STR_VERSION_MAJOR (0)
+#define UTF8STR_VERSION_MINOR (1)
+#define UTF8STR_VERSION_MICRO (0)
+
+/// \typedef utf8str_t
+/// \brief A type representing a string of UTF-8 characters
+/// \details utf8str_t is actually an array of utf8char_t's.
+typedef utf8char_t* utf8str_t;
+
+/// \brief Returns the number of UTF-8 characters (not including the terminating zero) in a string
+size_t utf8str_len(utf8str_t s)
+{
+	size_t len = 0;
+	while (1)
+	{
+		if (s[len] == 0)
+			return len;
+		else
+			len++;
+	}
+}
+
+/// \brief Copies characters from one string to another
+/// \param char_count The number of characters to copy
+/// \return **dst**
+utf8str_t copy_utf8str_n(utf8str_t dst, utf8str_t src, size_t char_count)
+{
+	return memcpy(dst, src, char_count * sizeof(utf8char_t));
+}
+
+/// \brief Copies characters from one string to another, up to and including the terminating zero
+/// \return **dst**
+utf8str_t copy_utf8str(utf8str_t dst, utf8str_t src)
+{
+	return copy_utf8str_n(dst, src, utf8str_len(src) + 1);
+}
+
+/// \brief Returns the number of bytes (including the terminating zero) the string would occupy in a buffer or a file
+/// \return ((size_t) -1), if **s** contains an invalid UTF-8 character.
+size_t utf8str_buf_size(utf8str_t s)
+{
+	size_t size = 0;
+	size_t i = 0;
+	while (1)
+	{
+		if (s[i] == 0)
+		{
+			return size + utf8char_size(0);
+		}
+		else
+		{
+			size_t tmp = utf8char_size(s[i]);
+			if (tmp == ((size_t) -1))
+				return tmp;
+			size += tmp;
+			i++;
+		}
+	}
+}
+
+/// \brief Returns the number of bytes the string would occupy in a buffer or a file
+/// \details This function may return before reaching **char_count** if terminating zero is met.
+/// \param char_count The number of characters to calculate the size of
+/// \return ((size_t) -1), if **s** contains an invalid UTF-8 character.
+size_t utf8str_buf_size_n(utf8str_t s, size_t char_count)
+{
+	size_t size = 0;
+	size_t i;
+	for (i = 0; i < char_count; i++)
+	{
+		if (s[i] == 0)
+		{
+			return size + utf8char_size(0);
+		}
+		else
+		{
+			size_t tmp = utf8char_size(s[i]);
+			if (tmp == ((size_t) -1))
+				return tmp;
+			size += tmp;
+		}
+	}
+	return size;
+}
+
+/// \brief Returns the number of bytes (including the terminating zero) the string would occupy in a buffer or a file
+/// \details This function returns the number of bytes up to the invalid UTF-8 character, if **s** contains one.
+size_t inval_utf8str_buf_size(utf8str_t s)
+{
+	size_t size = 0;
+	size_t i = 0;
+	while (1)
+	{
+		if (s[i] == 0)
+		{
+			return size + utf8char_size(0);
+		}
+		else
+		{
+			size_t tmp = utf8char_size(s[i]);
+			if (tmp == ((size_t) -1))
+				return size;
+			size += tmp;
+			i++;
+		}
+	}
+}
+
+/// \brief Writes a string into a buffer
+/// \details The buffer should be large enough to hold the whole string. The required buffer size can be found using utf8str_buf_size(**s**).
+/// \return The number of bytes actually written, if **s** does not contain an invalid UTF-8 character.
+/// \return ((size_t) -1), if **s** contains invalid UTF-8 character. The number of bytes already written into the buffer can be found using inval_utf8str_buf_size(**s**).
+size_t write_utf8str_to_buf(char *buf, utf8str_t s)
+{
+	size_t size = 0;
+	size_t i = 0;
+	while (1)
+	{
+		if (s[i] == 0)
+		{
+			return size + write_utf8char_to_buf(buf + size, 0);
+		}
+		else
+		{
+			size_t tmp = write_utf8char_to_buf(buf + size, s[i]);
+			if (tmp == ((size_t) -1))
+				return tmp;
+			size += tmp;
+			i++;
+		}
+	}
+}
+
+/// \brief Writes a string into a buffer
+/// \details The buffer should be large enough to hold utf8str_buf_size_n(**s**, **char_count**) bytes. This function may return before reaching **char_count** if terminating zero is met.
+/// \param char_count The number of characters to write
+/// \return The number of bytes actually written, if **s** does not contain an invalid UTF-8 character.
+/// \return ((size_t) -1), if **s** contains invalid UTF-8 character. The number of bytes already written into the buffer can be found using inval_utf8str_buf_size(**s**).
+size_t write_utf8str_to_buf_n(char *buf, utf8str_t s, size_t char_count)
+{
+	size_t size = 0;
+	size_t i;
+	for (i = 0; i < char_count; i++)
+	{
+		if (s[i] == 0)
+		{
+			return size + write_utf8char_to_buf(buf + size, 0);
+		}
+		else
+		{
+			size_t tmp = write_utf8char_to_buf(buf + size, s[i]);
+			if (tmp == ((size_t) -1))
+				return tmp;
+			size += tmp;
+		}
+	}
+	return size;
+}
+
+/// \brief Reads from a buffer into a string
+/// \details If **buf** contains an invalid UTF-8 character or a half one, the process is terminated without further processing. This function may return before reaching **bufsize** if terminating zero is met.
+/// \return The number of bytes actually consumed.
+size_t read_buf_to_utf8str(utf8str_t s, char *buf, size_t bufsize)
+{
+	size_t chars_consumed = 0;
+	size_t i;
+	for (i = 0; i < bufsize;)
+	{
+		size_t deduced_size = deduce_utf8char_size(buf[i]);
+		if (deduced_size == 0 || deduced_size == ((size_t) -1) || i + deduced_size > bufsize)
+			return i;
+
+		utf8char_t c = str_to_utf8char(buf + i);
+		if (c == ((utf8char_t) -1))
+			return i;
+
+
+		s[chars_consumed++] = c;
+		i += deduced_size;
+
+		if (c == 0)
+			break;
+	}
+	return bufsize;
+}
+
+#endif
